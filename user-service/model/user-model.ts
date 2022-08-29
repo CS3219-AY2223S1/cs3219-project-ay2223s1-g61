35fd77aa557fd3a2793336e81b2395c591311229
Chester Wong <psyforce99@gmail.com>
2022-08-29 23:17:04 +0800

    Add prettier formatter to project

--- user-service/model/user-model.ts
+++ user-service/model/user-model.ts
@@ -1,7 +1,7 @@
-import mongoose, { Model } from "mongoose";
-import bcrypt from "bcrypt";
-import validator from "validator";
-import uniqueValidator from "mongoose-unique-validator";
+import mongoose, { Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+import uniqueValidator from 'mongoose-unique-validator';
 
 var Schema = mongoose.Schema;
 
@@ -22,27 +22,29 @@ type UserModel = Model<IUser, {}, IUserMethods>;
 let UserModelSchema = new Schema<IUser, UserModel, IUserMethods>({
   username: {
     type: String,
-    required: [true, "Enter a username!"],
+    required: [true, 'Enter a username!'],
     unique: true,
     immutable: true,
     lowercase: true,
     trim: true,
     validate: [
       validator.isAlphanumeric,
-      "Usernames may only have letters and numbers!",
+      'Usernames may only have letters and numbers!',
     ],
   },
   password: {
     type: String,
-    required: [true, "Enter a password!"],
-    minLength: [4, "Password should be at least four characters!"],
+    required: [true, 'Enter a password!'],
+    minLength: [4, 'Password should be at least four characters!'],
   },
 });
 
-UserModelSchema.plugin(uniqueValidator, { message: "Username {VALUE} has already been taken :/" });
+UserModelSchema.plugin(uniqueValidator, {
+  message: 'Username {VALUE} has already been taken :/',
+});
 
 //schema middleware to apply before saving
-UserModelSchema.pre("save", async function (this: IUser, next) {
+UserModelSchema.pre('save', async function (this: IUser, next) {
   //hash the password, set hash cost to 10
   this.password = await bcrypt.hash(this.password, 10);
 
@@ -57,4 +59,4 @@ UserModelSchema.methods.correctPassword = async (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-export default mongoose.model<IUser>("UserModel", UserModelSchema);
+export default mongoose.model<IUser>('UserModel', UserModelSchema);
