7acdfec0dd9b62cf35df3162d6862c0e0ed32d70
seanjyjy <e0406739@u.nus.edu>
2022-09-04 02:51:24 +0800

    revert Signup page back to default

--- frontend/src/components/SignupPage.tsx
+++ frontend/src/components/SignupPage.tsx
@@ -2,77 +2,67 @@ import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, D
 import { useEffect, useState } from 'react';
 import useAuth from 'src/hooks/useAuth';
 import { isEmpty } from 'lodash';
-import MatchingModal from './MatchingModal';
 
 function SignupPage() {
-  const [open, setOpen] = useState(false);
+  const { registerUser, error } = useAuth();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState('');
+  const [dialogMsg, setDialogMsg] = useState<string[]>([]);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    await registerUser({ username, password });
+  };
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const setErrorDialog = (msgs: string[]) => {
+    setIsDialogOpen(true);
+    setDialogTitle('Error');
+    setDialogMsg(msgs);
+  };
+
+  useEffect(() => {
+    if (!isEmpty(error)) {
+      setErrorDialog(error);
+    }
+  }, [error]);
 
   return (
-    <div>
-      <button onClick={() => setOpen(!open)}>abc</button>
-      {open && <MatchingModal open={open} onClose={() => setOpen(false)} />}
-    </div>
+    <Box display={'flex'} flexDirection={'column'} width={'30%'}>
+      <Typography variant={'h3'} marginBottom={'2rem'}>
+        Sign Up
+      </Typography>
+      <TextField label="Username" variant="standard" value={username} onChange={(e) => setUsername(e.target.value)} sx={{ marginBottom: '1rem' }} autoFocus />
+      <TextField
+        label="Password"
+        variant="standard"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        sx={{ marginBottom: '2rem' }}
+      />
+      <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
+        <Button variant={'outlined'} onClick={handleSignup}>
+          Sign up
+        </Button>
+      </Box>
+
+      <Dialog open={isDialogOpen} onClose={closeDialog}>
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          {dialogMsg.map((msg) => {
+            return <DialogContentText>{msg}</DialogContentText>;
+          })}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
-
-  // const { registerUser, error } = useAuth();
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [isDialogOpen, setIsDialogOpen] = useState(false);
-  // const [dialogTitle, setDialogTitle] = useState('');
-  // const [dialogMsg, setDialogMsg] = useState<string[]>([]);
-
-  // const handleSignup = async (e) => {
-  //   e.preventDefault();
-  //   await registerUser({ username, password });
-  // };
-
-  // const closeDialog = () => setIsDialogOpen(false);
-
-  // const setErrorDialog = (msgs: string[]) => {
-  //   setIsDialogOpen(true);
-  //   setDialogTitle('Error');
-  //   setDialogMsg(msgs);
-  // };
-
-  // useEffect(() => {
-  //   if (!isEmpty(error)) {
-  //     setErrorDialog(error);
-  //   }
-  // }, [error]);
-
-  // return (
-  //   <Box display={'flex'} flexDirection={'column'} width={'30%'}>
-  //     <Typography variant={'h3'} marginBottom={'2rem'}>
-  //       Sign Up
-  //     </Typography>
-  //     <TextField label="Username" variant="standard" value={username} onChange={(e) => setUsername(e.target.value)} sx={{ marginBottom: '1rem' }} autoFocus />
-  //     <TextField
-  //       label="Password"
-  //       variant="standard"
-  //       type="password"
-  //       value={password}
-  //       onChange={(e) => setPassword(e.target.value)}
-  //       sx={{ marginBottom: '2rem' }}
-  //     />
-  //     <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
-  //       <Button variant={'outlined'} onClick={handleSignup}>
-  //         Sign up
-  //       </Button>
-  //     </Box>
-
-  //     <Dialog open={isDialogOpen} onClose={closeDialog}>
-  //       <DialogTitle>{dialogTitle}</DialogTitle>
-  //       <DialogContent>
-  //         {dialogMsg.map((msg) => {
-  //           return <DialogContentText>{msg}</DialogContentText>;
-  //         })}
-  //       </DialogContent>
-  //       <DialogActions>
-  //         <Button onClick={closeDialog}>Close</Button>
-  //       </DialogActions>
-  //     </Dialog>
-  //   </Box>
-  //);
 }
 
 export default SignupPage;
